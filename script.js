8dcb960717d32efcc84bd85869f52f1b7be4c23c
vincenth38 <vincent.heloin@gmail.com>
2021-10-19 21:17:57 -0700

    Update

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+      var principal = document.getElementById("principal").value;
+
+      var rate = document.getElementById("rate").value;
+      var years = document.getElementById("years").value;
+/*    Verify positive number is used*/
+      if (principal <=0 ) {
+        alert("Enter a positive number");
+        return false;}
+      else{
+
+      var interest = principal * years * rate /100;
+      var year = new Date().getFullYear()+parseInt(years);
+      document.getElementById("result").innerText=interest;}
+/*    Set the position back to the amount field*/
+      var input = document.getElementById ("principal");
+      input.focus ();
+
 }
-        
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
